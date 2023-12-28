2822efd1685f720de0f9dcc9b94c3d8bc46db13c
Dai Russi <dai.russi@gmail.com>
2023-12-28 13:34:42 -0300

    refinement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as HomeIcon, Search, LibraryBig, ChevronLeft, ChevronRight, Play, Heart } from 'lucide-react'
+import { Home as HomeIcon, Search, LibraryBig, ChevronLeft, ChevronRight, Play, Heart, Shuffle, SkipBack, SkipForward, Repeat, Laptop2, Mic2, LayoutList, Volume, Maximize2 } from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
@@ -97,33 +97,33 @@ export default function Home() {
           <div className='grid grid-cols-6 gap-8 mt-4'>
             <a href='#' className='p-2 flex flex-col gap-1 bg-white/5 rounded hover:bg-white/10'>
               <Image className='w-full rounded' src="/1.jpg" width={800} height={800} alt="capa album" />
-              <strong className='font-semibold'>Nome Album</strong>
-              <span className='text-sm text-zinc-500'>nome dos arti</span>
+              <strong className='font-semibold'>Nostalgia</strong>
+              <span className='text-sm text-zinc-500'>Ben B King</span>
             </a>
             <a href='#' className='p-2 flex flex-col gap-1 bg-white/5 rounded hover:bg-white/10'>
               <Image className='w-full rounded' src="/2.jpeg" width={800} height={800} alt="capa album" />
-              <strong className='font-semibold'>Nome Album</strong>
-              <span className='text-sm text-zinc-500'>nome dos arti</span>
+              <strong className='font-semibold'>Old Church</strong>
+              <span className='text-sm text-zinc-500'>Elevation Worship</span>
             </a>
             <a href='#' className='p-2 flex flex-col gap-1 bg-white/5 rounded hover:bg-white/10'>
               <Image className='w-full rounded' src="/3.jpg" width={800} height={800} alt="capa album" />
-              <strong className='font-semibold'>Nome Album</strong>
-              <span className='text-sm text-zinc-500'>nome dos arti</span>
+              <strong className='font-semibold'>Yeshua</strong>
+              <span className='text-sm text-zinc-500'>Julliany Souza</span>
             </a>
             <a href='#' className='p-2 flex flex-col gap-1 bg-white/5 rounded hover:bg-white/10'>
               <Image className='w-full rounded' src="/4.png" width={800} height={800} alt="capa album" />
-              <strong className='font-semibold'>Nome Album</strong>
-              <span className='text-sm text-zinc-500'>nome dos arti</span>
+              <strong className='font-semibold'>Joanne</strong>
+              <span className='text-sm text-zinc-500'>Lady Gaga</span>
             </a>
             <a href='#' className='p-2 flex flex-col gap-1 bg-white/5 rounded hover:bg-white/10'>
               <Image className='w-full rounded' src="/5.png" width={800} height={800} alt="capa album" />
-              <strong className='font-semibold'>Nome Album</strong>
-              <span className='text-sm text-zinc-500'>nome dos arti</span>
+              <strong className='font-semibold'>If I Were a Boy</strong>
+              <span className='text-sm text-zinc-500'>Beyonce</span>
             </a>
             <a href='#' className='p-2 flex flex-col gap-1 bg-white/5 rounded hover:bg-white/10'>
               <Image className='w-full rounded' src="/6.png" width={800} height={800} alt="capa album" />
-              <strong className='font-semibold'>Nome Album</strong>
-              <span className='text-sm text-zinc-500'>nome dos arti</span>
+              <strong className='font-semibold'>Top Hit</strong>
+              <span className='text-sm text-zinc-500'>Lady Gaga</span>
             </a>
           </div>
         </main>
@@ -135,10 +135,40 @@ export default function Home() {
             <strong className='font-semibold'>Nome Album</strong>
             <span className='text-xs text-zinc-500'>nome dos arti</span>
           </div>
-          <Heart />
+          <div> <Heart /></div>
         </div>
-        <div></div>
-        <div></div>
+        <div className='flex flex-col items-center gap-4'>
+          <div className='flex items-center gap-6'>
+            <Shuffle size={20} className='text-green-400' />
+            <SkipBack size={20} className='text-zinc-200' />
+            <button className='pl-1 w-10 h-10 rounded-full bg-white text-black flex justify-center items-center'>
+              <Play />
+            </button>
+            <SkipForward size={20} className='text-zinc-200' />
+            <Repeat size={20} className='text-zinc-200' />
+          </div>
+          <div className='flex items-center gap-2'>
+            <span className='text-xs text-zinc-400'>0:31</span>
+            <div className='h-1 rounded-full w-96 bg-zinc-600'>
+              <div className='h-1 rounded-full w-40 bg-zinc-200'></div>
+            </div>
+            <span className='text-xs text-zinc-400'>2:14</span>
+          </div>
+        </div>
+
+        <div className='flex items-center gap-4'>
+          <Mic2 size={20} />
+          <LayoutList size={20} />
+          <Laptop2 size={20} />
+          <div className='flex items-center gap-2'>
+            <Volume size={20} />
+            <div className='h-1 rounded-full w-24 bg-zinc-600'>
+              <div className='h-1 rounded-full w- bg-zinc-200'></div>
+            </div>
+          </div>
+          <Maximize2 size={20} />
+
+        </div>
       </footer>
     </div>
   )
